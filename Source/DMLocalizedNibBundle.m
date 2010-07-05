prabowomurti/sequel-pro

commit 42831cdc6761bf6020060d40216c771a7289f4e3

- Add support for localising menu item tooltips  - Add support for localising NSTextView value bindings' no selection placeholders, nil placeholders, not applicable placeholders and multiple selection placeholders
git-svn-id: http://sequel-pro.googlecode.com/svn/trunk@2418 eefc5464-0a4a-0410-82e6-d75a05d1654a

Source/DMLocalizedNibBundle.m
@@ -25,6 +25,7 @@
 + (void)_localizeLabelOfObject:(id)object table:(NSString *)table;
 @end
 
+static NSMutableArray *deliciousBindingKeys = nil;
 
 @implementation NSBundle (DMLocalizedNibBundle)
 
@@ -35,6 +36,12 @@
     NSAutoreleasePool *autoreleasePool = [[NSAutoreleasePool alloc] init];
     if (self == [NSBundle class]) {
         method_exchangeImplementations(class_getClassMethod(self, @selector(loadNibFile:externalNameTable:withZone:)), class_getClassMethod(self, @selector(deliciousLocalizingLoadNibFile:externalNameTable:withZone:)));
+        deliciousBindingKeys = [[NSMutableArray alloc] initWithObjects:
+                                    NSMultipleValuesPlaceholderBindingOption,
+                                    NSNoSelectionPlaceholderBindingOption,
+                                    NSNotApplicablePlaceholderBindingOption,
+                                    NSNullPlaceholderBindingOption,
+                                    nil];
     }
     [autoreleasePool release];
 }
@@ -114,6 +121,7 @@
     } else if ([object isKindOfClass:[NSMenuItem class]]) {
         NSMenuItem *menuItem = object;
         [self _localizeTitleOfObject:menuItem table:table];
+        [self _localizeToolTipOfObject:menuItem table:table];
         
         [self _localizeStringsInObject:[menuItem submenu] table:table];
         
@@ -168,6 +176,19 @@
             } else if ([view isKindOfClass:[NSTableView class]]) {
                 for (NSTableColumn *column in [view tableColumns]) {
                     [self _localizeStringValueOfObject:[column headerCell] table:table];
+                }
+            
+            } else if ([view isKindOfClass:[NSTextField class]]) {
+                NSDictionary *vb;
+                if (vb = [view infoForBinding:@"value"]) {
+                    NSMutableDictionary *lvb = [NSMutableDictionary dictionaryWithDictionary:[vb objectForKey:NSOptionsKey]];
+                    for (NSString *bindingKey in deliciousBindingKeys) {
+                        if ([lvb objectForKey:bindingKey] == [NSNull null]) continue;
+                        NSString *localizedBindingString = [self _localizedStringForString:[lvb objectForKey:bindingKey] table:table];
+                        if (localizedBindingString)
+                            [lvb setObject:localizedBindingString forKey:bindingKey];
+                    }
+                    [view bind:@"value" toObject:[vb objectForKey:NSObservedObjectKey] withKeyPath:[vb objectForKey:NSObservedKeyPathKey] options:lvb];
                 }
                 
             } else

commit 0bc94a9b72f2707d5d3da9a8166dcef61dd115a1

- Fix localisation of NSTextField labels - broken in r2418  - Add localisation support for NSSegmentedControl segment tooltips

Source/DMLocalizedNibBundle.m
@@ -169,6 +169,9 @@ static NSMutableArray *deliciousBindingKeys = nil;
                     NSString *localizedSegmentLabel = [self _localizedStringForString:[segmentedControl labelForSegment:segmentIndex] table:table];
                     if (localizedSegmentLabel)
                         [segmentedControl setLabel:localizedSegmentLabel forSegment:segmentIndex];
+                    NSString *localizedSegmentTooltip = [self _localizedStringForString:[[segmentedControl cell] toolTipForSegment:segmentIndex] table:table];
+                    if (localizedSegmentTooltip)
+                        [[segmentedControl cell] setToolTip:localizedSegmentTooltip forSegment:segmentIndex];
                     
                     [self _localizeStringsInObject:[segmentedControl menuForSegment:segmentIndex] table:table];
                 }
@@ -190,6 +193,7 @@ static NSMutableArray *deliciousBindingKeys = nil;
                     }
                     [view bind:@"value" toObject:[vb objectForKey:NSObservedObjectKey] withKeyPath:[vb objectForKey:NSObservedKeyPathKey] options:lvb];
                 }
+                [self _localizeStringsInObject:[control cell] table:table];
                 
             } else
                 [self _localizeStringsInObject:[control cell] table:table];